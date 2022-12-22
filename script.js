ec9ad865650f2f3d0d0b026c198118f443fee8e0
Lexicon404 <xba74@hotmail.com>
2022-12-22 14:04:05 +1100

    add isRead toggle

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ const formName = document.getElementById('name');
 const formAuthor = document.getElementById('author');
 
 const myLibrary = [
-  { name: 'Thor', author: 'Marvel' },
+  { name: 'Thor', author: 'Marvel', isRead: false },
 
-  { name: 'Spiderman', author: 'Peter Parker' },
+  { name: 'Spiderman', author: 'Peter Parker', isRead: false },
 ];
 
 function Book(name, author) {
   this.name = name;
   this.author = author;
+  this.isRead = false;
 }
 
 function addBookToLibrary(name, author) {
   const book = new Book(name, author);
   myLibrary.push(book);
-  console.log(myLibrary)
+  console.log(myLibrary);
 }
 
 function removeLibraryContent() {
@@ -34,13 +35,22 @@ function addRemoveButton(book, div) {
   div.appendChild(button);
 }
 
+function addIsReadButton(book, div) {
+  const read = document.createElement('button');
+  read.innerHTML = 'Read';
+  read.classList.add('book', 'read');
+  read.dataset.isRead = `${myLibrary.indexOf(myLibrary[book])}`;
+  div.appendChild(read);
+}
+
 function addLibraryContent() {
   for (const book in myLibrary) {
     const div = library.appendChild(document.createElement('div'));
-    div.textContent = `${myLibrary[book].name} ${myLibrary[book].author}`;
+    div.textContent = `${myLibrary[book].name} ${myLibrary[book].author} ${myLibrary[book].isRead}`;
+    addIsReadButton(book, div);
     addRemoveButton(book, div);
-}}
-
+  }
+}
 
 function listenRemoveButton() {
   const booksNodelist = document.querySelectorAll('[data-array-ref]');
@@ -52,9 +62,23 @@ function listenRemoveButton() {
   }));
 }
 
+function listenIsReadButton() {
+  const readNodelist = document.querySelectorAll('[data-is-read]');
+  const readArray = [...readNodelist];
+  readArray.forEach((button) => button.addEventListener('click', (e) => {
+    if (myLibrary[e.target.dataset.isRead].isRead === false) {
+      myLibrary[e.target.dataset.isRead].isRead = true;
+    } else {
+      myLibrary[e.target.dataset.isRead].isRead = false;
+    };
+    refreshLibraryContent()
+  }));
+}
+
 function refreshLibraryContent() {
   removeLibraryContent();
   addLibraryContent();
+  listenIsReadButton();
   listenRemoveButton();
 }
 
